11749f0c73e51ef1dc7d378b444c18a30bc6c7d0
Prateek Saxena <44098524+prateeks99@users.noreply.github.com>
2020-12-06 13:40:15 +0530

    Add files via upload

---
+++ src/components/Homepage/cards.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const card = ({ text }) => <div className="card">{text}</div>;
+
+export default card;
---
+++ src/components/Homepage/cards2.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const card = ({ text }) => <div className="card2"><a>{text}</a></div>;
+
+export default card;
